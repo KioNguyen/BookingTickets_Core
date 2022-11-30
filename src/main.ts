60de1c13b5a26f047b1a68e8b533c02ef6f56dcb
Kio Nguyen <vanhungtbt1998@gmail.com>
2022-11-30 13:44:09 +0700

    Config DB, app env, format env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Booking')
+    .setDescription('The booking API description')
+    .setVersion('1.0')
+    .addTag('booking')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.BOOKING_PORT).then(value => {
+    console.log("Booking app start on port ", process.env.BOOKING_CORE_PORT);
+  });
 }
 bootstrap();
